c9ef9914bd4674a2ece32f372b7c654f604fde37
Thomas Schorn <thomas@schorn.io>
2016-08-16 10:55:16 +0200

    [major] init server

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const PORT = process.env.PORT || 8080;
+
+const pkg = require('./package.json');
+const express = require('express');
+
+let app = express();
+
+app.use('/static', express.static(`${__dirname}/app`));
+
+app.get('/', (request, response) => {
+  response.sendFile(`${__dirname}/app/index.html`);
+});
+
+app.listen(PORT, () => {
+  console.log(`${pkg.name}v${pkg.version} listening on Port ${PORT}`);
+});
